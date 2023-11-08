30e5ee163b8091c8032a2451a21e334f77ce1b1e
Гришанов Клим <klim.grishanov@mail.ru>
2023-11-08 07:47:52 +0300

    Core: Solve Task 10

--- Task10/main.go
+++ Task10/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	data := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	data := []float64{-25.0, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
 
 	groups := make(map[int][]float64)
 	for i := 0; i < len(data); i++ {
