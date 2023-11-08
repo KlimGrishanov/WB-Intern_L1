4b9c393eb049c275b7f5cbabd3503e63ce3055cb
Гришанов Клим <klim.grishanov@mail.ru>
2023-11-08 07:07:10 +0300

    Core: Solve Task 8

---
+++ Task8/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func setBit(n int64, pos uint) int64 {
+	return n | (int64(1) << pos)
+}
+
+func deleteBit(n int64, pos uint) int64 {
+	return n &^ (int64(1) << pos)
+}
+
+func changeBitPos(n int64, pos uint, val bool) int64 {
+	var result int64
+	if val {
+		result = setBit(n, pos)
+	} else {
+		result = deleteBit(n, pos)
+	}
+	return result
+}
+
+func main() {
+	var i int64
+	i = 10
+	fmt.Println(changeBitPos(i, 2, true))
+	fmt.Println(changeBitPos(i, 3, false))
+}
