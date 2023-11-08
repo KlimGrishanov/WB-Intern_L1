d15564d8c17a93be5d821a4488c4833906e0b79a
Гришанов Клим <klim.grishanov@mail.ru>
2023-11-08 18:20:14 +0300

    Refactoring: Task 3

--- Task3/main.go
+++ Task3/main.go
@@ -11,13 +11,16 @@ func AsyncSquare(a int, out chan int) {
 func main() {
 	arr := [5]int{2, 4, 6, 8, 10}
 
+	// Создаем канал и закрываем его по разрушению стека Main
 	out := make(chan int)
 	defer close(out)
 
 	for i := 0; i < len(arr); i++ {
+		// Запуск нескольких воркеров
 		go AsyncSquare(arr[i], out)
 	}
 
+	// Считаем сумму из канала
 	var sum int
 	for i := 0; i < len(arr); i++ {
 		sum += <-out
