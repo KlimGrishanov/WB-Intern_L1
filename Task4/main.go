dfb3bf2c983785ea8a7134fc9a1d41ea38018e1b
Гришанов Клим <klim.grishanov@mail.ru>
2023-11-08 05:54:59 +0300

    Core: Solve Task 4

---
+++ Task4/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func worker(in chan int, ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			close(in)
+		default:
+			fmt.Println(<-in)
+		}
+	}
+}
+
+func main() {
+	var numOfWorker int
+	_, _ = fmt.Scanf("%d", &numOfWorker)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	in := make(chan int, 10)
+	defer close(in)
+
+	for i := 0; i < numOfWorker; i++ {
+		go worker(in, ctx)
+	}
+
+	i := 0
+	for {
+		in <- i
+		i++
+	}
+}
