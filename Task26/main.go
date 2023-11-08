b20b4c009be782364f2ca388d1d55481f873ae76
Гришанов Клим <klim.grishanov@mail.ru>
2023-11-08 18:16:53 +0300

    Core: Solve Task 26

---
+++ Task26/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func checkUniq(str string) bool {
+	uniqMap := make(map[rune]struct{})
+	str = strings.ToLower(str)
+	tempRuneArr := []rune(str)
+
+	for i := 0; i < len(tempRuneArr); i++ {
+		if _, isExist := uniqMap[tempRuneArr[i]]; isExist {
+			return false
+		}
+		uniqMap[tempRuneArr[i]] = struct{}{}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(checkUniq("abCdefAaf"))
+	fmt.Println(checkUniq("aabcd"))
+	fmt.Println(checkUniq("abcd"))
+}
